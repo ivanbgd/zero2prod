67dda83c2cdb6db49e42b259ac5146a7bdc0ce5a
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-10-01 23:51:36 +0200

    First use of actix-web and tokio - add hello and health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello, {}!", name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
