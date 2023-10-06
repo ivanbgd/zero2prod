787fe4bbc9adb3faf320043636bf8161e97dd250
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-10-06 23:51:58 +0200

    Add application data, use PgPool

--- src/startup.rs
+++ src/startup.rs
@@ -3,13 +3,20 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+/// Run the application - the web server - concurrently
+///
+/// Spin up a worker process for each available CPU core.
+/// Each worker runs its own copy of the application.
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())  // Get a pointer copy and attach it to the application state.
     })
         .listen(listener)?
         .run();
