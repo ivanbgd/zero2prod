665cdcbed8cbc5004d42669774a941fb740f534b
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-10-12 00:53:37 +0200

    Use TracingLogger instead of Logger

--- src/startup.rs
+++ src/startup.rs
@@ -2,11 +2,11 @@
 
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::web::{self, Data};
 use actix_web::{App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 /// Run the application - the web server - concurrently
 ///
@@ -16,7 +16,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone()) // Get a pointer copy and attach it to the application state.
