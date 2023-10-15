a419ec7d7ee1ba60015286782648cbbad16f68b9
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-10-15 21:34:16 +0200

    Add "domain.rs" and `SubscriberName`

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,10 +1,9 @@
 //! src/routes/subscriptions.rs
 
-use crate::consts::{FORBIDDEN_NAME_CHARACTERS, MAX_NAME_LEN};
+use crate::domain::SubscriberName;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
-use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -30,29 +29,18 @@ pub async fn subscribe(
     web::Form(form): web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    if !is_valid_name(&form.name) {
-        return HttpResponse::BadRequest().finish();
-    }
+    let subscriber_name = SubscriberName::parse(form.name.clone());
+
+    // Kept just as an exercise and to demonstrate the most basic form of validation,
+    // which is, unfortunately, not good enough in practice.
+    // if !is_valid_name(&form.name) {
+    //     return HttpResponse::BadRequest().finish();
+    // }
 
     match insert_subscriber(&form, &pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
-}
-
-/// Checks validity of a new user's name
-///
-/// Returns `true` if **ALL** validation constraints are satisfied,
-/// `false` otherwise.
-pub fn is_valid_name(name: &str) -> bool {
-    let is_empty_or_whitespace = name.trim().is_empty();
-
-    let is_too_long = name.graphemes(true).count() > MAX_NAME_LEN;
-
-    let contains_a_forbidden_character =
-        name.chars().any(|c| FORBIDDEN_NAME_CHARACTERS.contains(&c));
-
-    !(is_empty_or_whitespace || is_too_long || contains_a_forbidden_character)
 }
 
 /// Insert the new subscriber details in a Postgres database
@@ -91,53 +79,4 @@ pub async fn insert_subscriber(form: &FormData, pool: &PgPool) -> Result<(), sql
     })?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use once_cell::sync::Lazy;
-    use rstest::rstest;
-
-    static VALID_MAX_LONG_NAME: Lazy<String> = Lazy::new(|| "å".repeat(MAX_NAME_LEN));
-    static TOO_LONG_NAME: Lazy<String> = Lazy::new(|| "a".repeat(MAX_NAME_LEN + 1));
-
-    #[rstest(
-        valid_name,
-        case::first_name("John"),
-        case::first_last("John Doe"),
-        case::first_last_whitespace("  \t \n  John  \t \n  Doe \t \n  "),
-        case::non_ascii("å"),
-        case::non_ascii_max_long(&VALID_MAX_LONG_NAME),
-        case::punctuation(". , ? ! : ;"),
-    )]
-    fn is_valid_name_passes_valid_names(valid_name: &'static str) {
-        let is_valid = is_valid_name(valid_name);
-        assert_eq!(true, is_valid, "Rejected a valid name '{}'.", valid_name);
-    }
-
-    #[rstest(
-        invalid_name,
-        error_message,
-        case::empty_name("", "empty"),
-        case::whitespace_name(" \t \r \n   ", "whitespace"),
-        case::too_long(&TOO_LONG_NAME, "too long"),
-        case::forward_slash("/", "forward slash"),
-        case::open_parenthesis("(", "open parenthesis"),
-        case::close_parenthesis(")", "close parenthesis"),
-        case::double_quote(r#"""#, "double quote"),
-        case::open_angle_bracket("<", "open angle bracket"),
-        case::close_angle_bracket(">", "close angle bracket"),
-        case::back_slash("\\", "back slash"),
-        case::open_curly_brace("{", "open curly brace"),
-        case::close_curly_brace("}", "close curly brace"),
-    )]
-    fn is_valid_name_rejects_invalid_names(invalid_name: &'static str, error_message: &str) {
-        let is_valid = is_valid_name(invalid_name);
-        assert_eq!(
-            false, is_valid,
-            "Didn't reject the invalid name '{}' (name is {}).",
-            invalid_name, error_message
-        );
-    }
 }
