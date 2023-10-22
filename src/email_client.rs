c7f3e3ff8fbd8a946d29163457930dd8e4adea70
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-10-22 15:59:14 +0200

    Add `serde_json`; enhance UT to check body (implement a wiremock::Matcher)

--- src/email_client.rs
+++ src/email_client.rs
@@ -57,7 +57,7 @@ impl EmailClient {
     }
 
     pub async fn send_email(
-        self: &Self,
+        &self,
         recipient: SubscriberEmail,
         subject: &str,
         html_content: &str,
@@ -86,6 +86,7 @@ impl EmailClient {
 }
 
 #[derive(serde::Serialize)]
+#[serde(rename_all = "PascalCase")]
 struct SendEmailRequest {
     from: String,
     to: String,
@@ -103,18 +104,44 @@ mod tests {
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
     use secrecy::Secret;
-    use wiremock::matchers::any;
-    use wiremock::{Mock, MockServer, ResponseTemplate};
+    use wiremock::matchers::{header, header_exists, method, path};
+    use wiremock::{Match, Mock, MockServer, Request, ResponseTemplate};
+
+    struct SendEmailBodyMatcher;
+
+    impl Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &Request) -> bool {
+            // Try to parse the body as a JSON value
+            let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
+
+            // Check that all mandatory fields are populated without inspecting the field values
+            match result {
+                Ok(body) => {
+                    dbg!(&body);
+                    body.get("From").is_some()
+                        && body.get("To").is_some()
+                        && body.get("Subject").is_some()
+                        && body.get("HtmlBody").is_some()
+                        && body.get("TextBody").is_some()
+                }
+                Err(_) => false,
+            }
+        }
+    }
 
     #[tokio::test]
-    async fn send_email_fires_a_request_to_base_url() {
+    async fn send_email_sends_the_expected_request() {
         //Arrange
         let mock_server = MockServer::start().await;
         let base_url = mock_server.uri();
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let email_client = EmailClient::new(base_url, sender, Secret::new(Faker.fake()));
 
-        Mock::given(any())
+        Mock::given(header_exists("X-Postmark-Server-Token"))
+            .and(header("Content-Type", "application/json"))
+            .and(path("/email"))
+            .and(method("POST"))
+            .and(SendEmailBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .named("Trigger email provider")
